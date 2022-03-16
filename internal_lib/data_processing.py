acf5aa4c393829270275961c842d1a95edac042a
Sprizgola <gionathan.desogus@gmail.com>
2022-03-17 00:14:12 +0100

    Raggruppate funzioni di parse

--- internal_lib/data_processing.py
+++ internal_lib/data_processing.py
@@ -1,7 +1,10 @@
 import re
 import logging
+
 import numpy as np
 import pandas as pd
+
+from datetime import timedelta
 
 
 def parse_raw_log_data(input_path: str, output_path: str, verbose: bool = True) -> None:
@@ -23,28 +26,61 @@ def parse_raw_log_data(input_path: str, output_path: str, verbose: bool = True) 
             continue
         tmp_dict["ip"] = m.group(1)
         tmp_dict["timestamp"] = m.group(2)
-        tmp_dict["http_method"] = m.group(3)
-        tmp_dict["request"] = m.group(4)
-        tmp_dict["http_version"] = m.group(5)
-        tmp_dict["status"] = m.group(6)
-        tmp_dict["size"] = m.group(7)
-        tmp_dict["referer"] = m.group(8)
-        tmp_dict["user_agent"] = m.group(9)
+        tmp_dict["http_method"] = m.group(3) if m.group(3) != "-" else None
+        tmp_dict["request"] = m.group(4) if m.group(4) != "-" else None
+        tmp_dict["http_version"] = m.group(5) if m.group(5) != "-" else None
+        tmp_dict["status"] = m.group(6) if m.group(6) != "-" else None
+        tmp_dict["size"] = m.group(7) if m.group(7) != "-" else None
+        tmp_dict["referer"] = m.group(8) if m.group(8) != "-" else None
+        tmp_dict["user_agent"] = m.group(9) if m.group(9) != "-" else None
 
         dict_list.append(tmp_dict)
 
     pd.DataFrame(dict_list).to_csv(output_path, index=False)
 
 
+def find_sessions(df: pd.DataFrame):
+    df["status"] = df["status"].astype(str)
+
+    df.loc[df["size"] == "-", "size"] = 0
+    df.loc[:, "size"] = df["size"].astype(int)
+    df.loc[df["referer"] == "-", "referer"] = None
+    df.loc[df["user_agent"] == "-", "user_agent"] = None
+    df.loc[:, "timestamp"] = pd.to_datetime(df["timestamp"], format="%d/%b/%Y:%H:%M:%S %z", utc=True)
+    df["ref_date"] = df["timestamp"].apply(lambda x: x.date())
+
+    # Devo raggruppare i dati in sessioni
+    df["req_duration"] = df.groupby(["ip", "user_agent"])["timestamp"].shift(-1) - \
+                         df.groupby(["ip", "user_agent"])["timestamp"].shift(0)
+
+    df.loc[df["req_duration"] > timedelta(minutes=30), "req_duration"] = timedelta(minutes=30)
+
+    # FIXME: Verificare se assegnare 0 o 30
+    df["req_duration"] = df["req_duration"].apply(lambda x: x.total_seconds() if not pd.isna(x) else 0)
+
+    df["new_session"] = df.groupby(["ip", "user_agent"])["timestamp"].diff()
+    df["new_session"] = np.where((df["new_session"] > timedelta(minutes=30)) | (df["new_session"].isnull()), 1, 0)
+
+    df["session_id"] = df.sort_values(["ip", "user_agent"])["new_session"].cumsum()
+
+    return df
+
 def extract_features(df: pd.DataFrame) -> pd.DataFrame:
 
-    # Calcolo i valori aggregati
-    df_feature = df.groupby(["session_id"]).agg(
-        session_duration=("timestamp", np.ptp), request_count=("request", "count"),
-        mean_req=("req_duration", np.mean), total_req_size=("size", "sum"))
+    # Session duration
+    df_feature = df.groupby(["session_id"])["timestamp"].agg(session_duration=np.ptp)
+
+    # Request count
+    df_feature["requests_count"] = df.groupby(["session_id"])["request"].count()
+
+    # Mean request duration
+    df_feature["mean_request"] = df.groupby(["session_id"])["req_duration"].mean()
+
+    # Total request size (byte)
+    df_feature["total_size"] = df.groupby(["session_id"])["size"].sum()
 
     df_feature["session_duration"] = df_feature["session_duration"].apply(lambda x: x.total_seconds())
-    df_feature.loc[df_feature["session_duration"] == 0, "session_duration"] = 30*60
+    # df_feature.loc[df_feature["session_duration"] == 0, "session_duration"] = 30*60
 
     # La funzione 'count' considera solo i valori non 'nan'
     empty_referer = df.groupby(["session_id"])["timestamp"].count() - df.groupby(["session_id"])["referer"].count()
@@ -80,16 +116,14 @@ def extract_features(df: pd.DataFrame) -> pd.DataFrame:
     PARTE RELATIVA ALLE FEATURE SULL'E-COMMERCE
     """
 
-    carrello = ["cart.html", "carrello.html", "warenkorb.html", "panier.html", "cesta.html"]
     cart_actions = ["aggiungi_al_carrello.php"]
     login_operations = ["action=login", "msg=login-error"]
     internal_search_engine = ["search.html\?q", "search-by-bike.html\?"]
     source = ["utm_source"]
-    checkout = ["checkout.php"]
-
     # TODO: Number of views of pages informing about the store and the trading company -> Non presente
     # TODO: Number of views of pages with entertainment contents -> Non presente
     # TODO: Number of other page views -> Da chiarire
+    # TODO: Whether the session ended with a purchase
 
     # Number of login operations (including “Register success” and “Login success”)
     df["login_actions"] = df["request"].str.contains("|".join(login_operations))
@@ -124,52 +158,4 @@ def extract_features(df: pd.DataFrame) -> pd.DataFrame:
     df_feature["homepage_views"] = df.groupby("session_id")["homepage_views"].sum()
 
     return df_feature
-
-
-def extract_marketing_features(df: pd.DataFrame) -> pd.DataFrame:
-    carrello = ["cart.html", "carrello.html", "warenkorb.html", "panier.html", "cesta.html"]
-    cart_actions = ["aggiungi_al_carrello.php"]
-    login_operations = ["action=login", "msg=login-error"]
-    internal_search_engine = ["search.html\?q", "search-by-bike.html\?"]
-    source = ["utm_source"]
-    checkout = ["checkout.php"]
-
-    # TODO: Number of views of pages informing about the store and the trading company -> Non presente
-    # TODO: Number of views of pages with entertainment contents -> Non presente
-    # TODO: Number of other page views -> Da chiarire
-
-    # Number of login operations (including “Register success” and “Login success”)
-    df["login_actions"] = df["request"].str.contains("|".join(login_operations))
-    df_feature = df.groupby("session_id")["login_actions"].sum().reset_index()
-
-    # Number of searches using the internal search engine
-    df["internal_search"] = df["request"].str.contains("|".join(internal_search_engine))
-    df_feature["internal_search"] = df.groupby("session_id")["internal_search"].sum().fillna(0)
-
-    # Number of operations of adding a product to the shopping cart
-    df["add_to_cart"] = df["request"].str.contains("|".join(cart_actions))
-    df_feature["add_to_cart"] = df.groupby("session_id")["add_to_cart"].sum().fillna(0)
-
-    # Whether a “source” of the session is specified
-    df["has_source"] = df["request"].str.contains("|".join(source))
-    df_feature["has_source"] = df.groupby("session_id")["has_source"].sum() > 0
-
-    # TODO: Whether the session ended with a purchase
-    # braintree.php?lang = it & payment = yes
-
-    # Number of views of product description pages -> currency=[a-z A-Z]{3}&size=[0-9 A-Z]{1,3}
-    df["product_views"] = df["request"].str.contains("currency=[a-z A-Z]{3}&size=[0-9 A-Z]{1,3}")
-    df_feature["product_views"] = df.groupby("session_id")["product_views"].sum()
-
-    # Number of views of the page with shipping terms and conditions
-    df["conditions_views"] = df["request"].str.contains("conditions-of-sale.html")
-    df_feature["conditions_views"] = df.groupby("session_id")["conditions_views"].sum()
-
-    # Number of views of the website’s home page
-    homepage_regex = "^\/[a-z]{2}\/$"
-    df["homepage_views"] = df["request"].str.contains(homepage_regex)
-    df_feature["homepage_views"] = df.groupby("session_id")["homepage_views"].sum()
-
-    return df_feature
-
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import logging
 
 from datetime import datetime, timedelta
-from internal_lib.data_processing import parse_raw_log_data, extract_features
+from internal_lib.data_processing import find_sessions, extract_features
 
 
 INPUT_PATH = "data/log.txt"
@@ -18,33 +18,129 @@ start_time = datetime.now()
 # parse_raw_log_data(input_path=INPUT_PATH, output_path=OUTPUT_PATH, verbose=True)
 
 df = pd.read_csv(OUTPUT_PATH)
-df["status"] = df["status"].astype(str)
 
-df.loc[df["size"] == "-", "size"] = 0
-df.loc[:, "size"] = df["size"].astype(int)
-df.loc[df["referer"] == "-", "referer"] = None
-df.loc[df["user_agent"] == "-", "user_agent"] = None
-df.loc[:, "timestamp"] = pd.to_datetime(df["timestamp"], format="%d/%b/%Y:%H:%M:%S %z", utc=True)
-df["ref_date"] = df["timestamp"].apply(lambda x: x.date())
-
-# Devo raggruppare i dati in sessioni
-df["req_duration"] = df.groupby(["ip", "user_agent"])["timestamp"].shift(-1) - df.groupby(["ip", "user_agent"])["timestamp"].shift(0)
-df.loc[df["req_duration"] > timedelta(minutes=30), "req_duration"] = timedelta(minutes=30)
-# I valori 'nan' son dovuti alle sessioni 'nuove'; setto quindi come valore 30*60 (30m) a queste sessioni
-df["req_duration"] = df["req_duration"].apply(lambda x: x.total_seconds() if not pd.isna(x) else 30*60)
-
-df["new_session"] = df.groupby(["ip", "user_agent"])["timestamp"].diff()
-df["new_session"] = np.where((df["new_session"] > timedelta(minutes=30)) | (df["new_session"].isnull()), 1, 0)
-
-
-COLUMNS_TO_GROUP = ["ip", "session_id", "user_agent"]
-
-# TODO: AGGIUNGERE USER AGENT AL GROUP BY
-df["session_id"] = df.sort_values(["ip", "user_agent"])["new_session"].cumsum()
-
+df = find_sessions(df)
 
 df_features = extract_features(df)
 
+# print(f"Esecuzione terminata in: {datetime.now() - start_time}")
 
+# X = df_features.values
 
-print(f"Esecuzione terminata in: {datetime.now() - start_time}")
+# from sklearn.cluster import KMeans
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+# from sklearn.metrics import silhouette_samples, silhouette_score
+#
+# km = KMeans(
+#     n_clusters=4, init='k-means++',
+#     random_state=42, verbose=2, tol=0.0000001, max_iter=1000)
+#
+# km.fit(X)
+#
+# range_n_clusters = [2, 3, 4, 5, 6]
+#
+# for n_clusters in range_n_clusters:
+#     # Create a subplot with 1 row and 2 columns
+#     fig, (ax1, ax2) = plt.subplots(1, 2)
+#     fig.set_size_inches(18, 7)
+#
+#     # The 1st subplot is the silhouette plot
+#     # The silhouette coefficient can range from -1, 1 but in this example all
+#     # lie within [-0.1, 1]
+#     ax1.set_xlim([-0.1, 1])
+#     # The (n_clusters+1)*10 is for inserting blank space between silhouette
+#     # plots of individual clusters, to demarcate them clearly.
+#     ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+#
+#     # Initialize the clusterer with n_clusters value and a random generator
+#     # seed of 10 for reproducibility.
+#     clusterer = KMeans(n_clusters=n_clusters, random_state=42, verbose=2)
+#     cluster_labels = clusterer.fit_predict(X)
+#
+#     # The silhouette_score gives the average value for all the samples.
+#     # This gives a perspective into the density and separation of the formed
+#     # clusters
+#     silhouette_avg = silhouette_score(X, cluster_labels)
+#     print(
+#         "For n_clusters =",
+#         n_clusters,
+#         "The average silhouette_score is :",
+#         silhouette_avg,
+#     )
+#
+#     # Compute the silhouette scores for each sample
+#     sample_silhouette_values = silhouette_samples(X, cluster_labels)
+#
+#     y_lower = 10
+#     for i in range(n_clusters):
+#         # Aggregate the silhouette scores for samples belonging to
+#         # cluster i, and sort them
+#         ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+#
+#         ith_cluster_silhouette_values.sort()
+#
+#         size_cluster_i = ith_cluster_silhouette_values.shape[0]
+#         y_upper = y_lower + size_cluster_i
+#
+#         color = cm.nipy_spectral(float(i) / n_clusters)
+#         ax1.fill_betweenx(
+#             np.arange(y_lower, y_upper),
+#             0,
+#             ith_cluster_silhouette_values,
+#             facecolor=color,
+#             edgecolor=color,
+#             alpha=0.7,
+#         )
+#
+#         # Label the silhouette plots with their cluster numbers at the middle
+#         ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+#
+#         # Compute the new y_lower for next plot
+#         y_lower = y_upper + 10  # 10 for the 0 samples
+#
+#     ax1.set_title("The silhouette plot for the various clusters.")
+#     ax1.set_xlabel("The silhouette coefficient values")
+#     ax1.set_ylabel("Cluster label")
+#
+#     # The vertical line for average silhouette score of all the values
+#     ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+#
+#     ax1.set_yticks([])  # Clear the yaxis labels / ticks
+#     ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+#
+#     # 2nd Plot showing the actual clusters formed
+#     colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+#     ax2.scatter(
+#         X[:, 0], X[:, 1], marker=".", s=30, lw=0, alpha=0.7, c=colors, edgecolor="k"
+#     )
+#
+#     # Labeling the clusters
+#     centers = clusterer.cluster_centers_
+#     # Draw white circles at cluster centers
+#     ax2.scatter(
+#         centers[:, 0],
+#         centers[:, 1],
+#         marker="o",
+#         c="white",
+#         alpha=1,
+#         s=200,
+#         edgecolor="k",
+#     )
+#
+#     for i, c in enumerate(centers):
+#         ax2.scatter(c[0], c[1], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
+#
+#     ax2.set_title("The visualization of the clustered data.")
+#     ax2.set_xlabel("Feature space for the 1st feature")
+#     ax2.set_ylabel("Feature space for the 2nd feature")
+#
+#     plt.suptitle(
+#         "Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"
+#         % n_clusters,
+#         fontsize=14,
+#         fontweight="bold",
+#     )
+#
+# plt.show()
