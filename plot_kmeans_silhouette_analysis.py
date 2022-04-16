715bd32bdd482071008f52fefc52e71cf3252b0a
Gionathan Desogus <gionathan.d@queryo.com>
2022-04-16 18:43:03 +0200

    aggiunto file per esplorazione clusters

---
+++ plot_kmeans_silhouette_analysis.py
@@ -0,0 +1,169 @@
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import pandas as pd
+
+def outlier_treatment(datacolumn):
+ sorted(datacolumn)
+ Q1,Q3 = np.percentile(datacolumn , [25,75])
+ IQR = Q3 - Q1
+ lower_range = Q1 - (1.5 * IQR)
+ upper_range = Q3 + (1.5 * IQR)
+ return lower_range,upper_range
+
+np.random.seed(42)
+
+
+df = pd.read_csv("./parsed_data/parsed_data_1.csv", index_col="session_id")
+
+# upper_lim = 60 * 60
+#
+# df.loc[df['session_duration'] >= upper_lim, "session_duration"] = upper_lim
+
+# upper_lim = df['session_duration'].quantile(.95)
+# lower_lim = df['session_duration'].quantile(.05)
+#
+# df = df[(df['session_duration'] < upper_lim) & (df['session_duration'] >= lower_lim)]
+
+df.fillna(0, inplace=True)
+
+# Effettuo lo scaling delle variabili non categoriche
+numerical_features = ['session_duration', 'requests_count', 'mean_request', 'total_size',
+                      'pc_referer', 'pc_error_4xx', 'pc_head_req', 'pg_img_ratio',
+                      'page_views', 'pc_page_ref_empty', 'login_actions', 'internal_search',
+                      'add_to_cart', 'has_source', 'product_views', 'homepage_views']
+
+index = df.index.to_numpy().reshape((df.shape[0], 1))
+data = df[numerical_features].values
+cat_col = df.has_source.values.reshape(df.shape[0], 1)
+
+
+# scaler = MinMaxScaler()
+scaler = StandardScaler()
+# scaler = RobustScaler()
+
+scaler.fit(data)
+
+X_scaled = scaler.transform(data)
+X = np.concatenate([index, X_scaled, cat_col], axis=1)
+
+# Prendo un sottoinsieme di X
+np.random.shuffle(X)
+
+X = X[:50000, 1:]
+
+# X = X[:100000]
+
+range_n_clusters = [2, 3, 4, 5, 6]
+
+for n_clusters in range_n_clusters:
+    # Create a subplot with 1 row and 2 columns
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+
+    # The 1st subplot is the silhouette plot
+    # The silhouette coefficient can range from -1, 1 but in this example all
+    # lie within [-0.1, 1]
+    ax1.set_xlim([-0.1, 1])
+    # The (n_clusters+1)*10 is for inserting blank space between silhouette
+    # plots of individual clusters, to demarcate them clearly.
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+    # Initialize the clusterer with n_clusters value and a random generator
+    # seed of 10 for reproducibility.
+    clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+    cluster_labels = clusterer.fit_predict(X)
+
+    # The silhouette_score gives the average value for all the samples.
+    # This gives a perspective into the density and separation of the formed
+    # clusters
+    silhouette_avg = silhouette_score(X, cluster_labels)
+    print(
+        "For n_clusters =",
+        n_clusters,
+        "The average silhouette_score is :",
+        silhouette_avg,
+    )
+
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        # Aggregate the silhouette scores for samples belonging to
+        # cluster i, and sort them
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10  # 10 for the 0 samples
+
+    ax1.set_title("The silhouette plot for the various clusters.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    # 2nd Plot showing the actual clusters formed
+    colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(
+        X[:, 0], X[:, 1], marker=".", s=30, lw=0, alpha=0.7, c=colors, edgecolor="k"
+    )
+
+    # Labeling the clusters
+    centers = clusterer.cluster_centers_
+    # Draw white circles at cluster centers
+    ax2.scatter(
+        centers[:, 0],
+        centers[:, 1],
+        marker="o",
+        c="white",
+        alpha=1,
+        s=200,
+        edgecolor="k",
+    )
+
+    for i, c in enumerate(centers):
+        ax2.scatter(c[0], c[1], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("Feature space for the 1st feature")
+    ax2.set_ylabel("Feature space for the 2nd feature")
+
+    plt.suptitle(
+        "Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"
+        % n_clusters,
+        fontsize=14,
+        fontweight="bold",
+    )
+
+plt.show()
+
+print()
